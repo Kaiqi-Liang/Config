52c47f2cdd5e4f1df04bf28c45607bc3020e099e
Kaiqi Liang <kaiqi.liang@outlook.com>
2020-12-24 14:36:44 +1100

    Add comments

--- config.sh
+++ config.sh
@@ -1,18 +1,11 @@
 #!/bin/zsh
 # Set up ssh key if not exist
 ssh=~/.ssh
-if mkdir ~/.ssh || ! test -f ~/.ssh/id_rsa
+if mkdir $ssh || ! test -f ~/.ssh/id_rsa
 then
     ssh-keygen
     cat $ssh/id_rsa.pub
 fi
-
-mkdir -p ~/.ghc
-for config in [a-z]*[!.]?? [sg][sh][ch]/*
-do
-    echo creating ~/.$config
-    ln -i $config ~/.$config
-done
 
 # Set up vim spell dictionary
 spell=vim/spell
@@ -21,4 +14,12 @@ for file in $spell/*
 do
     echo creating ~/.$spell
     ln -i $file ~/.$spell
+done
+
+# Set up the rest of the config files
+mkdir -p ~/.ghc
+for config in [a-z]*[!.]?? [sg][sh][ch]/*
+do
+    echo creating ~/.$config
+    ln -i $config ~/.$config
 done
